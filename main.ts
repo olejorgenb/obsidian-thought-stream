836ea00a69b7f17fa17e7831135897b2ce20e372
Ole Jørgen Brønner <olejorgenb@yahoo.no>
2021-09-12 12:27:34 +0200

    playing around

--- main.ts
+++ main.ts
@@ -1,22 +1,80 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { findSourceMap } from 'module';
+import { App, MarkdownEditView, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, View } from 'obsidian';
 
-interface MyPluginSettings {
+interface ThoughtStreamSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: ThoughtStreamSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+/**
+ * Checks if a note is tagged with a specific tag
+ */
+function isTagged(app: App, tag: string) {
+	let active = app.workspace.getActiveFile()
+	let pageCache = app.metadataCache.getFileCache(active)
+	if (!tag.startsWith("#")) {
+		tag = "#" + tag
+	}
+	return Boolean(pageCache.tags?.find(t => t.tag === tag))
+}
+
+interface Thought {
+	timestamp: Date
+	content: string
+}
+
+function parseThought(line: string) {
+	const dateRe = /^([0-9]{4}-[1-9]-[1-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9])\t(.*)/
+
+	const match = dateRe.exec(line)
+	if (match) {
+		const [timestampString, content] = match
+		const timestamp = Date.parse(timestampString)
+		return {
+			timestamp, content
+		}
+
+	} else {
+		return
+	}
+
+}
+
+function isMarkdownView(view: View): view is MarkdownView {
+	return view.getViewType() === "markdown"
+}
+
+export default class ThoughtStream extends Plugin {
+	settings: ThoughtStreamSettings;
+
+	cm: CodeMirror.Editor
+
+	async lockLines(doc: CodeMirror.Doc) {
+
+		doc.markText(
+			{ line: 0, ch: 0 },
+			{ line: doc.lineCount() - 1, ch: 0 },
+			{
+				readOnly: true
+			}
+		)
+/* 		doc.eachLine(line => {
+			if (parseThought(line.text)) {
+				const n = doc.getLineNumber(line)
+				doc.markText({line: n, ch: 0}, {line: n+1, ch: 0})
+			}
+		}) */
+	}
 
 	async onload() {
 		console.log('loading plugin');
 
 		await this.loadSettings();
-
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
+// asdf asdf
+/* 		this.addRibbonIcon('dice', 'Sample Plugin', () => {
 			new Notice('This is a notice!');
 		});
 
@@ -39,18 +97,38 @@ export default class MyPlugin extends Plugin {
 				return false;
 			}
 		});
-
+ */
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
+			console.log('codemirror', cm)
+			this.cm = cm
 		});
 
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
 
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEvent(this.app.workspace.on("file-open", this.initialize.bind(this)));
+
+		this.lockLines(this.cm.getDoc())
+
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	async initialize() {
+		if (isTagged(this.app, "#stream")) {
+			let active = this.app.workspace.getActiveFile()
+			let pageCache = this.app.metadataCache.getFileCache(active)
+
+			const leaf = this.app.workspace.activeLeaf
+			window.__leaf = leaf
+			const view = this.app.workspace.activeLeaf.view
+
+			if (isMarkdownView(view)) {
+				this.lockLines(this.cm.getDoc())
+			}
+		}
 	}
 
 	onunload() {
@@ -83,9 +161,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: ThoughtStream;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ThoughtStream) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
